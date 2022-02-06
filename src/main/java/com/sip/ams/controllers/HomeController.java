dadd903e8cdc64c7e4a026ec6ef47843302f4765
sadokbenaouicha <sadokbenaouicha@gmail.com>
2022-02-06 14:42:02 +0100

    premier projet spring MVC

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController {
+	@RequestMapping("/info")
+	public String info()
+	{
+		System.out.println("sasaa");
+		return "home/info";
+	}
+	
+	@RequestMapping("/affichage")
+	public String aff()
+	{
+		return "home/affichage";
+	}
+	
+}
